cb94cedc19c5afd94c05a132b22935d60c8550b5
Brocier <jschoe04@gmail.com>
2018-11-01 09:30:20 -0400

    Adding Netlify Identity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import './App.css';
+
+import netlifyIdentity from 'netlify-identity-widget'
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      brainhealth: 100,
+      contentment: 100,
+      bodyhealth: 100,
+      serotonininsystem: 0,
       clicks: 0
     };
     this.addClick = this
       .addClick
       .bind(this);
+
+    this.handleLogIn = this
+      .handleLogIn
+      .bind(this);
+
     // setInterval(this.addClick, 1000);
   }
   addClick() {
@@ -16,11 +28,20 @@ class App extends Component {
       clicks: state.clicks + 1
     }));
   }
-
+  handleLogIn() {
+    // You can import the widget into any component and interact with it.
+    netlifyIdentity.open()
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+
+          <div>
+            <button onClick={this.handleLogIn}>Log in with netlify</button>
+          </div>
+          {/* <div data-netlify-identity-menu></div>
+          <div data-netlify-identity-button>Login with Netlify Identity</div> */}
 
           <p className="Super-center">Serotonin
             <img
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import netlifyIdentity from 'netlify-identity-widget';
+
+window.netlifyIdentity = netlifyIdentity
+// You must run this once before trying to interact with the widget
+netlifyIdentity.init()
 
 ReactDOM.render(
   <App/>, document.getElementById('root'));
